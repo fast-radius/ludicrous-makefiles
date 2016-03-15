d62e530c50877bd9dea763b49ce48e8726ab3202
Martin Walsh <github@mwalsh.org>
2016-03-15 17:41:33 -0500

    fix parenthetical bug

--- includes/bundler.mk
+++ includes/bundler.mk
@@ -5,7 +5,7 @@ BUNDLE_INSTALL_OPTS ?=
 
 Gemfile.lock: Gemfile FORCE | _program_bundle
 	@bundle check &> /dev/null && \
-		$(call _log,rubygems up-to-date) || \
+		( $(call _log,rubygems up-to-date) ) || \
 		( $(call _log,installing rubygems); \
 		  bundle install $(BUNDLE_INSTALL_OPTS) )
 
