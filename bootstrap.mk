d19c0a04f96be01ee62eedf1dccec682a1842d20
Martin Walsh <github@mwalsh.org>
2017-04-09 11:46:58 -0500

    download the help script when bootstrapping

--- bootstrap.mk
+++ bootstrap.mk
@@ -1,13 +1,14 @@
 .DEFAULT_GOAL := Makefile
 
 INCLUDES_DIR := .makefiles
-DOWNLOAD_URL := https://raw.githubusercontent.com/martinwalsh/ludicrous-makefiles/master/includes/ludicrous.mk
+DOWNLOAD_BRANCH := master
+DOWNLOAD_URL := https://raw.githubusercontent.com/martinwalsh/ludicrous-makefiles/$(DOWNLOAD_BRANCH)/includes
 
 $(INCLUDES_DIR):
 	mkdir -p $(INCLUDES_DIR)
 
-$(INCLUDES_DIR)/ludicrous.mk: | $(INCLUDES_DIR)
-	curl -Lso $@ $(DOWNLOAD_URL)
+$(INCLUDES_DIR)/ludicrous.mk $(INCLUDES_DIR)/help.awk: | $(INCLUDES_DIR)
+	curl -Lso $@ $(DOWNLOAD_URL)/$(notdir $@)
 
-Makefile: | $(INCLUDES_DIR)/ludicrous.mk
+Makefile: | $(INCLUDES_DIR)/ludicrous.mk $(INCLUDES_DIR)/help.awk
 	@echo 'include $(INCLUDES_DIR)/ludicrous.mk' > Makefile
